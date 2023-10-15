8099725e95f68612c2d6d82ca538c56f33f2a488
Kyueun Lee <noyarim@gmail.com>
2023-10-14 22:59:26 -0700

    updated exercise solution

--- Exercise_solution.R
+++ Exercise_solution.R
@@ -28,11 +28,11 @@ OpenSEIRS<-function(t, state, parameters) {
 }
 
 #2. Define parameters and starting compartment sizes
-parameters <- c(beta = 0.9, #effective contact rate (aka transmission rate)
-                gamma = 1/3, #recovery rate (1/duration infection)
-                birth = 0.01, #birth rate (per capita)
-                death = 0.01, #all-cause mortality rate
-                omega = 1/(365*3), # waning immunity
+parameters <- c(beta = 0.2, #effective contact rate (aka transmission rate)
+                gamma = 1/14, #recovery rate (1/duration infection)
+                birth = 12/1000/365,#0.0000027, #birth rate (per capita)
+                death = 12/1000/365, #all-cause mortality rate
+                omega = 1/(30.5*3),#0.01, # waning immunity
                 t_lat = 5 # latent period from E
                 
 )
@@ -48,11 +48,11 @@ T_end <- 300 #run model for 300 time steps (e.g. days)
 times <- seq(0, T_end, by = 1) #runs the model for 300 time steps (e.g. days), and computes output at each time step 
 
 #Run the base-case with beta = 0.5
-parameters['beta'] <- 0.5
+parameters['beta'] <- 0.2
 out_lowbeta <- data.frame(ode(y = state, times = times, func = OpenSEIRS, parms = parameters))
 out_lowbeta$totI <- out_lowbeta$E + out_lowbeta$I
 #Run the base-case with beta = 0.5
-parameters['beta'] <- 0.9
+parameters['beta'] <- 0.8
 out_highbeta <- data.frame(ode(y = state, times = times, func = OpenSEIRS, parms = parameters))
 out_highbeta$totI <- out_highbeta$E + out_highbeta$I
 
@@ -60,13 +60,13 @@ out_highbeta$totI <- out_highbeta$E + out_highbeta$I
 out_lowbeta_t <- melt(out_lowbeta, id.vars='time')
 ggplot(out_lowbeta_t)+
   geom_line(aes(time,value,color=variable))+
-  ggtitle("Beta = 0.5")+
+  ggtitle("Beta = 0.2")+
   theme_bw()
 
 out_highbeta_t <- melt(out_highbeta, id.vars='time')
 ggplot(out_highbeta_t)+
   geom_line(aes(time,value,color=variable))+
-  ggtitle("Beta = 0.9")+
+  ggtitle("Beta = 0.8")+
   theme_bw()
 
 # Plot E + I over time & peak of epidemics
@@ -75,11 +75,9 @@ ggplot(totI)+
   geom_line(aes(time,lowbeta, color='low beta'))+
   geom_line(aes(time,highbeta, color='high beta'))+  
   ylab("E + I")+
-  annotate(geom="vline", 
-           x = c(which.max(totI$lowbeta),which.max(totI$highbeta)),
-           xintercept = c(which.max(totI$lowbeta),which.max(totI$highbeta)),
-           linetype = c("dashed","dashed"),
-           color = c("#0072B2","#D55E00"))+
+  geom_vline(xintercept = c(which.max(totI$lowbeta),which.max(totI$highbeta)),
+             linetype = c("dashed","dashed"),
+             color = c("#0072B2","#D55E00"))+
   annotate(geom="text",
            label = c(as.character(which.max(totI$lowbeta)),as.character(which.max(totI$highbeta))),
            x = c(which.max(totI$lowbeta),which.max(totI$highbeta)),
